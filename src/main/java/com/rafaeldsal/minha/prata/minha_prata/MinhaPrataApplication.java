acc154d834d13d7558d56fa083c86b5d5ae6407c
Rafael Souza <rafaelsnt123@gmail.com>
2025-03-24 15:01:12 -0300

    created project and add docker-compose file with mysql configuration

---
+++ src/main/java/com/rafaeldsal/minha/prata/minha_prata/MinhaPrataApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldsal.minha.prata.minha_prata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaPrataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaPrataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaeldsal/minha/prata/minha_prata/MinhaPrataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldsal.minha.prata.minha_prata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaPrataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
